b742548846c707bb55d4bd69641d404d710c1208
Nicholas Goehner <nicholasgoehner@Nicholass-MacBook-Pro.local>
2014-11-02 11:26:58 -0800

    updated Labels to be blue and stuff

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
 
     
     override func viewDidLoad() {
@@ -38,7 +39,18 @@ class ViewController: UIViewController {
         enterMessageTextField.resignFirstResponder()
         
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
-    }
+        
+        // Code for Name Label
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+     }
+    
+    
 
 }
 
